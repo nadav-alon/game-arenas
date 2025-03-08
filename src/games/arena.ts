1e50b8261e981639715369726b5e7839be816655
Nadav Alon <alonadav2000@gmail.com>
2025-03-08 22:15:30 +0200

    some pr fixes (that gpt gave me)

--- src/games/arena.ts
+++ src/games/arena.ts
@@ -26,36 +26,48 @@ class Arena<V extends readonly Vertex[] = [], E extends readonly [string, string
 
     addEdge<NewE extends [V[number]['v'], V[number]['v']]>(...newEdge: NewE): Arena<V, [...E, NewE]> {
       if (this.compiled) throw new Error('already compiled')
-        const ret = new Arena<V, [...E, NewE]>(this.vertices, [...this.edges, [...newEdge]])
-        return ret
+
+      const [from, to] = newEdge;
+      if (!this.vertices.some(v => v.v === from) || !this.vertices.some(v => v.v === to)) {
+        throw new Error(`Cannot add edge ${from} → ${to}: One or both vertices do not exist`);
+      }
+      
+      return new Arena<V, [...E, NewE]>(this.vertices, [...this.edges, [...newEdge]]);
     }
 
     getNeighbors<Vert extends V[number]['v']>(vertex: Vert): NeighborsOf<Vert, V, E> {
-        return this.edges.filter(e => e[0] === vertex).map(e => e[1]) as NeighborsOf<Vert, V, E>
+      if (this.compiled) {
+        return (this.adjacencyList.get(vertex) ?? []) as NeighborsOf<Vert, V, E>;
+      }
+
+      return this.edges.filter(e => e[0] === vertex).map(e => e[1]) as NeighborsOf<Vert, V, E>; 
     }
 
     map: C extends true ? Map<V[number]['v'], V[number]> : never
+    adjacencyList: C extends true ? Map<V[number]['v'], V[number]['v'][]> : never
 
     _setMap(m: typeof this.map) {
       this.map = m
     }
 
     compile() {
-      this.compiled = true as C
-      const map = new Map<V[number]['v'], V[number]>(this.vertices.map(v=>[v.v, v]))
+      this.compiled = true as C;
+      const map = new Map<V[number]['v'], V[number]>(this.vertices.map(v => [v.v, v]));
+      
+      const adjacencyList = new Map<V[number]['v'], V[number]['v'][]>();
+      this.edges.forEach(([from, to]) => {
+        if (!adjacencyList.has(from)) adjacencyList.set(from, []);
+        adjacencyList.get(from)!.push(to);
+      });
 
-      const ret = (this as Arena<V, E, true>)
-      ret._setMap(map)
-
-      return ret
+      const ret = this as Arena<V, E, true>;
+      ret._setMap(map);
+      ret.adjacencyList = adjacencyList;
+      return ret;
     }
 
     get<Vert extends V[number]['v']>(vertex: Vert):SpecificVertexOf<Vert, V> {
-      if (!this.compiled){
-        return this.vertices.find(v=>v.v === vertex) as SpecificVertexOf<Vert, V>
-      }
-      
-      return this.map.get(vertex) as SpecificVertexOf<Vert, V>
+      return (this.map?.get(vertex) ?? this.vertices.find(v => v.v === vertex)) as SpecificVertexOf<Vert, V>;
     }
 
     subArena<NewVert extends readonly V[number]['v'][]>(newV: NewVert): Arena<SpecificVerticesOf<NewVert, V>, EdgesThatStartAndEndAtVertices<NewVert, E>, true> {
