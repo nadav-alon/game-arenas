39c4f2539e1b70558ad91f8c4aee8b755fd4fe82
Nadav Alon <alonadav2000@gmail.com>
2025-03-09 12:31:25 +0200

    some game changes

--- src/games/game.ts
+++ src/games/game.ts
@@ -8,18 +8,18 @@ export class Game<V extends readonly Vertex[] = [], E extends Edges = []> {
     winCondition: WinCondition<V>;
     history: V[number][]
 
-    constructor(vertices: V = [] as unknown as V, edges: E = [] as unknown as E, initialState: V[number], winCondition:WinCondition<V>) {
-        this.arena = new Arena(vertices, edges).compile()
-        this.currentState = initialState
+    constructor(arena: typeof this.arena, initialState: V[number]['v'], winCondition:WinCondition<V>) {
+        this.arena = arena
+        this.currentState = this.arena.get(initialState)
         this.winCondition = winCondition
-        this.history = [initialState]
+        this.history = [this.currentState]
     }
 
     getCurrentPlayer(): Player {
         return this.currentState.p
     }
 
-    play(choice: NeighborsOf<typeof this.currentState.v,V,E>[number]){ 
+    play<CurV extends typeof this.currentState.v>(choice: NeighborsOf<CurV, typeof this.arena.vertices, typeof this.arena.edges>[number]) { 
         const newState = this.arena.getNeighbors(this.currentState.v).find(n=> n === choice)
         if (!newState){
             throw new Error('cannot play this choice') 
@@ -33,3 +33,11 @@ export class Game<V extends readonly Vertex[] = [], E extends Edges = []> {
         return this.winCondition(this.history)
     }
 }
+
+
+const arena = new Arena().addP0('1').addP1('2').addEdge("1","2").compile()
+const game = new Game(arena, '1', (p)=>p.find(v=>v.v === '1') ? 0 : 1)
+
+game.play<typeof game.currentState.v>("2")
+game.getCurrentWinner()
+// game.play<typeof game.currentState.v>()
