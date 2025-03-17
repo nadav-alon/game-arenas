7129f6a8cb538a4227c32ac712c76c0af13f157f
Nadav Alon <alonadav2000@gmail.com>
2025-03-17 21:52:32 +0200

    add todo type tests

--- src/games/arena.test.ts
+++ src/games/arena.test.ts
@@ -23,49 +23,64 @@ describe('Arena', () => {
             expect(bNeighbors.length).toBe(0)
         })
 
-        // it('testing subarena', () => {
-        //     const arena = new Arena().
-        //         addP0('0').
-        //         addP0('1').
-        //         addP0('2').
-        //         addP0('3').
-        //         addP0('4').
-        //         addP0('5').
-        //         addP0('6').
-        //         addP0('7').
-        //         addP0('8').
-        //         addP0('9').
-        //         addP0('10').
-        //         addEdge('0','1').
-        //         addEdge('0','2').
-        //         addEdge('0','3').
-        //         addEdge('0','4').
-        //         addEdge('0','5').
-        //         addEdge('0','6').
-        //         addEdge('0','7').
-        //         addEdge('0','8').
-        //         addEdge('0','9').
-        //         addEdge('0','10').
-        //         addEdge('1','0').
-        //         addEdge('2','0').
-        //         addEdge('3','0').
-        //         addEdge('4','0').
-        //         addEdge('5','0').
-        //         addEdge('6','0').
-        //         addEdge('7','0').
-        //         addEdge('8','0').
-        //         addEdge('9','0').
-        //         addEdge('10','0').compile()
+        it('testing subarena', () => {
+            const arena = new Arena().
+                addP0('0').
+                addP0('1').
+                addP0('2').
+                addP0('3').
+                addP0('4').
+                addP0('5').
+                addP0('6').
+                addP0('7').
+                addP0('8').
+                addP0('9').
+                addP0('10').
+                addEdge('0', '1').
+                addEdge('0', '2').
+                addEdge('0', '3').
+                addEdge('0', '4').
+                addEdge('0', '5').
+                addEdge('0', '6').
+                addEdge('0', '7').
+                addEdge('0', '8').
+                addEdge('0', '9').
+                addEdge('0', '10').
+                addEdge('1', '0').
+                addEdge('2', '0').
+                addEdge('3', '0').
+                addEdge('4', '0').
+                addEdge('5', '0').
+                addEdge('6', '0').
+                addEdge('7', '0').
+                addEdge('8', '0').
+                addEdge('9', '0').
+                addEdge('10', '0').compile()
 
-        //     const subArena = arena.subArena(['0','1'] as ['0','1'])
+            const subArena = arena.subArena(['0', '1'] as ['0', '1'])
 
-        //     expect(subArena.vertices.map(v=>v.v)).toEqual(expect.arrayContaining(['0','1']))
+            expect(subArena.vertices.map(v => v.v)).toEqual(expect.arrayContaining(['0', '1']))
 
-        //     expect(subArena.edges).toEqual(expect.arrayContaining([['0','1'],['1','0']]))
-        //     expect(subArena.edges.length).toBe(2)
+            expect(subArena.edges).toEqual(expect.arrayContaining([['0', '1'], ['1', '0']]))
+            expect(subArena.edges.length).toBe(2)
 
 
-        // })
+        })
 
+    })
+    describe('Type Tests', () => {
+        describe('Constant', () => {
+            it.todo('Building', () => { })
+            it.todo('Get', () => { })
+            it.todo('Neighbors', () => { })
+            it.todo('Subarena', () => { })
+        })
+
+        describe('Variable', () => {
+            it.todo('Building', () => { })
+            it.todo('Get', () => { })
+            it.todo('Neighbors', () => { })
+            it.todo('Subarena', () => { })
+        })
     })
 })
