3bf54ef79a24e6adb4c770604bfa1aef340e1f1a
Nadav Alon <alonadav2000@gmail.com>
2025-03-17 21:56:56 +0200

    add game todo tests

---
+++ src/games/game.test.ts
@@ -0,0 +1,16 @@
+import { describe, it } from "vitest";
+
+describe('Game', () => {
+    describe('Reachability', () => {
+        it.todo('Plays correctly', () => { })
+        it.todo('Correct winning strategy', () => { })
+    })
+    describe('Buchi', () => {
+        it.todo('Plays correctly', () => { })
+        it.todo('Correct winning strategy', () => { })
+    })
+    describe('Parity', () => {
+        it.todo('Plays correctly', () => { })
+        it.todo('Correct winning strategy', () => { })
+    })
+})
