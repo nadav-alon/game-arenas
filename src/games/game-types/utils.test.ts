36ad107471fa038a182da9d646b263e53b5bb552
Nadav Alon <alonadav2000@gmail.com>
2025-04-05 19:22:53 +0300

    add todo utils tests

---
+++ src/games/game-types/utils.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "vitest";
+
+describe('Game Utils', () => {
+    it.todo('Strategy From History')
+    it.todo('Next Step')
+    it.todo('Strategy Loop Start Point')
+    it.todo('Loop States')
+})
